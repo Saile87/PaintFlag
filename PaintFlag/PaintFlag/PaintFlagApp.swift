91f48c934af1fb3ef6796f726d974caf9c76ce4e
Elias Breitenbach <elias.b@me.com>
2024-02-02 21:55:31 +0100

    Create a project on github

---
+++ PaintFlag/PaintFlag/PaintFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintFlagApp.swift
+//  PaintFlag
+//
+//  Created by Elias Breitenbach on 02.02.24.
+//
+
+import SwiftUI
+
+@main
+struct PaintFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
